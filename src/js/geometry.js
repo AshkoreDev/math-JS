c18f671f62b913fd967b6d72f5a71dbfa054693f
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-17 22:44:19 +0100

    Added basics formulas

---
+++ src/js/geometry.js
@@ -0,0 +1,46 @@
+
+// SQUARE
+const squareSide = 5;
+
+
+function calculateSquare(squareSide) {
+
+	return {
+		perimeter:squareSide * 4,
+		area: squareSide * squareSide,
+	}
+
+}
+
+// console.log( '%c squareArea', 'color: blue; font-size: 20px;');
+
+
+// TRIANGLE
+const triangleSideA = 6;
+const triangleSideB = 6;
+const triangleBase = 4;
+const triangleHeight = 5.5;
+
+
+function calculateTriangle(triangleSideA, triangleSideB, triangleBase, triangleHeight) {
+
+	return {
+		perimeter: triangleSideA + triangleSideB + triangleBase,
+		area: (triangleBase * triangleHeight) / 2,
+	}
+
+}
+
+// CIRCLE
+const circleRadio = 3;
+
+
+function calculateCircle(circleRadio) {
+
+	return {
+		diameter: Math.pow(circleRadio, 2),
+		circumference : circleDiameter * Math.PI,
+		area: (circleRadio ** 2) * Math.PI,
+	}
+
+}
