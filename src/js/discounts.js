2a26f43cc8df52a03470670286eb7261bde0aa07
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-19 22:38:50 +0100

    Added coupons logic

--- src/js/discounts.js
+++ src/js/discounts.js
@@ -1,7 +1,18 @@
-const calculateBtn = document.getElementById('calculateBtn');
+const calculateDiscountBtn = document.getElementById('calculateDiscountBtn');
 const price = document.getElementById('price');
 const discount = document.getElementById('discount');
-const result = document.getElementById('result');
+const resultDiscount = document.getElementById('resultDiscount');
+
+const calculateCouponBtn = document.getElementById('calculateCouponBtn');
+const priceCoupon = document.getElementById('priceCoupon');
+const coupon = document.getElementById('coupon');
+const resultCoupon = document.getElementById('resultCoupon');
+
+const coupons = [
+	{ coupon: 'basic', discount: 15 },
+	{ coupon: 'platinum', discount: 30 },
+	{ coupon: 'golden', discount: 50 }
+];
 
 
 function calculateDiscount() {
@@ -13,17 +24,52 @@ function calculateDiscount() {
 
 	if (!newPrice || !newDiscount) {
 
-		result.innerText = `Debes ingresar los datos requeridos.`;
+		resultDiscount.innerText = `Debes ingresar los datos requeridos.`;
 
 	} else if (newDiscount > 100) {
 		
-		result.innerText = `El descuento no puede ser mayor a 100.`;
+		resultDiscount.innerText = `El descuento no puede ser mayor a 100.`;
 
 	} else {
 
-		result.innerText = `El precio con un descuento de ${newDiscount}% es de $${totalPrice}.`;
+		resultDiscount.innerText = `El precio con un descuento de ${newDiscount}% es de $${totalPrice}.`;
 	}
 
 }
 
-calculateBtn.addEventListener('click', calculateDiscount);
+function calculateCoupon() {
+
+	const newPrice = Number(priceCoupon.value);
+	const newCoupon = coupon.value;
+	let discountCoupon = 0;
+
+	if (!newPrice || newCoupon == '') {
+
+		resultCoupon.innerText = `Debes ingresar los datos requeridos.`;
+
+	} else {
+
+		coupons.forEach(item => {
+
+			if (newCoupon == item.coupon) {
+				
+				discountCoupon = item.discount;
+				console.log(discountCoupon);
+			}
+		});
+
+		if (discountCoupon == 0) {
+
+			resultCoupon.innerText = `El cupón no existe.`;
+
+		} else {
+
+			const totalPriceCoupon = (newPrice * (100 - discountCoupon)) / 100;
+
+			resultCoupon.innerText = `El precio con el cupón ${newCoupon} es de $${totalPriceCoupon}.`;
+		}
+	}
+}
+
+calculateDiscountBtn.addEventListener('click', calculateDiscount);
+calculateCouponBtn.addEventListener('click', calculateCoupon);
