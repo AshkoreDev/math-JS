ea1b6a2f0b68275ff8a74219516f857537af7d69
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-21 20:43:04 +0100

    Added median logic

--- src/js/statistics.js
+++ src/js/statistics.js
@@ -6,35 +6,37 @@ function calculateAverage(list) {
 	const sum = list.reduce((acum, newValor) => acum + newValor);
 
 	const average = sum / list.length;
-	return `El promedio es ${average}.`;
+	return average;
 }
 
 
 // MEDIAN
-
-let listTwo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; 
+let listTwo = [10,20,30,40,10]; 
 
 function isEven(list) {
 
 	return !(list.length % 2);
 }
 
-function isOdd(list) {
-
-	return list.length % 2;
-}
 
 function calculateMedian(list) {
 
-	// calculateAverage(list);
 	const listIsEven = isEven(list);
 
 	if (listIsEven) {
 		
+		const indexMediumOne = list[(list.length / 2) - 1];
+		const indexMediumTwo = list[list.length / 2];
+
+		const listMedium = [indexMediumOne, indexMediumTwo];
+		const averageMedium = calculateAverage(listMedium);
+		return averageMedium;
 
 	} else {
-		const indexOddMedian = Math.floor(list.length / 2) + 1;
-		console.log(list[indexOddMedian];
+
+		const indexOddMedian = Math.floor(list.length / 2);
+		const oddMedian = list[indexOddMedian];
+		return oddMedian;
 	}
 }
 
